2a4e454a8aeab8f884a682971afe12c27e9307b8
dominhthanh0206 <dominhthanh0206@gmail.com>
2023-01-14 16:51:17 +0700

    be

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import config from "config";
+import connect from "./until/connect";
+const port = config.get<number>("port");
+
+const app = express();
+app.listen(port, async () => {
+  console.log("App is running");
+  await connect();
+});
